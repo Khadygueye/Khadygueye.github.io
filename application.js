e4c660506a428a5994743b3ad5baec3e9c3992ab
Khadygueye <khadyg7626@kippnyccp.org>
2016-05-25 12:16:59 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,7 +1,39 @@
-$(document).ready(function(){
-  
-  $("p").click(function(){
-    $(this).css("opacity","0.5");
+$(document).ready(function() {
+  $("#music_button").click(function() {
+    $(".content").css("display", "none");
+    $(".music").css("display", "inline-block");
   })
-  
+
+})
+
+$(document).ready(function() {
+  $("#movies_button").click(function() {
+    $(".content").css("display", "none");
+    $(".movies").css("display", "inline-block");
+  })
+
+})
+
+$(document).ready(function() {
+  $("#food_button").click(function() {
+    $(".content").css("display", "none");
+    $(".food").css("display", "inline-block");
+  })
+
+})
+
+$(document).ready(function() {
+  $("#books_button").click(function() {
+    $(".content").css("display", "none");
+    $(".books").css("display", "inline-block");
+  })
+
+})
+
+$(document).ready(function() {
+  $("#tv-shows_button").click(function() {
+    $(".content").css("display", "none");
+    $(".tv_shows").css("display", "inline-block");
+  })
+
 })
