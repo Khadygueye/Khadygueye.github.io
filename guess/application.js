de6062a12c8863bc462c15f0a0c50d4fa7a69e65
Khadygueye <khadyg7626@kippnyccp.org>
2016-06-09 12:13:14 -0400

    Update application.js

--- guess/application.js
+++ guess/application.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
       var random_num = Math.floor((Math.random() * 100) + 1);
-      //create click event for submit button
       $("#submit").click(function() {
-            var guess = $("input[name="
-              user_guess "]").val();
-            if guess = random_num {
+            var guess =
+              $("input[name=user_guess]").val();
+            if (guess == random_num) {
+              ("You Got It!!")
               $("#response").html("<p>You Got It!!</p>");
-              else if guess < random_num {
+            } else if (guess < random_num) {
+                alert("You're close. Guess higher.")
                 $("#response").html("<p>You're close. Guess higher.</p>");
-                else :
+            } else {
                   $("#response").html("<p>You're close. Guess higher.</p>");
-
-              }
             }
+      });
+})
