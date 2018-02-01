3152e3b322dd6f3f93d6ef45997123f5dd9f36be
Khadygueye <khadyg7626@kippnyccp.org>
2018-02-01 09:03:09 -0500

    Update application.js

--- application.js
+++ application.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
-  //$("#Fiction").hide();
-  //$("#NonFiction").hide();
-  //$("#AutoBiography").hide();
-  //$("#SciFi").hide();
-  //$("#Poetry").hide();
+  $("#Fiction").hide();
+  $("#NonFiction").hide();
+  $("#AutoBiography").hide();
+  $("#SciFi").hide();
+  $("#Poetry").hide();
   $(".bc").css("display", "inline-block");
-  //$("#Fiction_button").click(function() {
-    //$(".content").css("display", "none");
-    //$(".Fiction").css("display", "inline-block");
-  //});
-  //$("#AutoBio_button").click(function() {
-    //$(".content").css("display", "none");
-    //$(".AutoBigraphy").css("display", "inline-block");
-  //});
-  //$("#SciFi_button").click(function() {
-    //$(".content").css("display", "none");
-    //$(".SciFi").css("display", "inline-block");
-  //});
-   //$("#Poetry").click(function() {
-    //$(".content").css("display", "none");
-    //$(".books").css("display", "inline-block");
-  //});
-  //$("#tv-shows_button").click(function() {
-    //$(".content").css("display", "none");
-    //$(".tv_shows").css("display", "inline-block");
-  //});
+  $("#Fiction_button").click(function() {
+    $(".content").css("display", "none");
+    $(".Fiction").css("display", "inline-block");
+  });
+  $("#AutoBio_button").click(function() {
+    $(".content").css("display", "none");
+    $(".AutoBigraphy").css("display", "inline-block");
+  });
+  $("#SciFi_button").click(function() {
+    $(".content").css("display", "none");
+    $(".SciFi").css("display", "inline-block");
+  });
+   $("#Poetry").click(function() {
+    $(".content").css("display", "none");
+    $(".books").css("display", "inline-block");
+  }};
+  $("#tv-shows_button").click(function() {
+    $(".content").css("display", "none");
+    $(".tv_shows").css("display", "inline-block");
+  });
 
 })
 
